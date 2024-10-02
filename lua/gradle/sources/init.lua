76f5199f6118546a20d9b34867d827f0720a2f89
oclay <ocleidyreve@gmail.com>
2024-10-02 18:57:05 -0400

    feat: add source for project data

---
+++ lua/gradle/sources/init.lua
@@ -0,0 +1,91 @@
+local scan = require('plenary.scandir')
+local Project = require('gradle.sources.project')
+local GradleConfig = require('gradle.config')
+local Path = require('plenary.path')
+local SettingsParser = require('gradle.parsers.settings_gradle_parser')
+local TasksParser = require('gradle.parsers.tasks_parser')
+local DependencyTreeParser = require('gradle.parsers.dependency_tree_parser')
+local CommandBuilder = require('gradle.utils.cmd_builder')
+local Utils = require('gradle.utils')
+local console = require('gradle.utils.console')
+
+local build_gradle_file_pattern = '.*build.gradle'
+
+local M = {}
+
+local create_custom_commands = function()
+  local custom_commands = {}
+  for index, custom_command in ipairs(GradleConfig.options.custom_commands) do
+    custom_commands[index] =
+      Project.Command(custom_command.name, custom_command.description, custom_command.cmd_args)
+  end
+  return custom_commands
+end
+
+local create_project_from_file = function(build_gradle_path)
+  local build_gradle_file = Path:new(build_gradle_path)
+  local build_gradle_parent = build_gradle_file:parent()
+  local project_path = build_gradle_parent:absolute()
+  local project_name = string.match(project_path, '%' .. Path.path.sep .. '(%w+)$')
+  local modules_names = {}
+  local settings_gradle = Path:new(build_gradle_parent, 'settings.gradle')
+  local settings_gradle_kts = Path:new(build_gradle_parent, 'settings.gradle.kts')
+  if settings_gradle:exists() then
+    local setting = SettingsParser.parse_file(settings_gradle:absolute())
+    project_name = setting.project_name
+    modules_names = setting.modules_names
+  elseif settings_gradle_kts:exists() then
+    local setting = SettingsParser.parse_file(settings_gradle_kts:absolute())
+    project_name = setting.project_name
+    modules_names = setting.modules_names
+  end
+  return Project.new(project_path, build_gradle_path, project_name)
+end
+
+---Load the gradle projects given a directory
+---@param base_path string
+---@return Project[]
+M.scan_projects = function(base_path)
+  local projects = {}
+  local custom_commands = create_custom_commands()
+  scan.scan_dir(base_path, {
+    search_pattern = build_gradle_file_pattern,
+    depth = 10,
+    on_insert = function(build_gradle_path, _)
+      local project = create_project_from_file(build_gradle_path)
+      project:set_commands(custom_commands)
+      table.insert(projects, project)
+    end,
+  })
+  return projects
+end
+
+M.load_project_tasks = function(project_path, callback)
+  local _callback = function(state, job)
+    local tasks
+    if Utils.SUCCEED_STATE == state then
+      local content_lines = job:result()
+      tasks = TasksParser.parse(content_lines)
+    end
+    callback(state, tasks)
+  end
+  local command = CommandBuilder.build_gradle_tasks_cmd(project_path)
+  local show_output = GradleConfig.options.console.show_tasks_load_execution
+  console.execute_command(command.cmd, command.args, show_output, _callback)
+end
+
+M.load_project_dependencies = function(project_path, callback)
+  local _callback = function(state, job)
+    local dependencies
+    if Utils.SUCCEED_STATE == state then
+      local output_lines = job:result()
+      dependencies = DependencyTreeParser.parse(output_lines)
+    end
+    callback(state, dependencies)
+  end
+  local command = CommandBuilder.build_gradle_dependencies_cmd(project_path)
+  local show_output = GradleConfig.options.console.show_dependencies_load_execution
+  console.execute_command(command.cmd, command.args, show_output, _callback)
+end
+
+return M
---
+++ lua/gradle/sources/project.lua
@@ -0,0 +1,190 @@
+local Utils = require('gradle.utils')
+
+---@class Project
+---@field id string
+---@field build_gradle_path string
+---@field root_path string
+---@field name string
+---@field tasks Project.Task[]
+---@field dependencies Project.Dependency[]
+---@field commands Project.Command[]
+local Project = {}
+Project.__index = Project
+
+---Create a new instance of a Project
+---@param root_path string
+---@param build_gradle_path string
+---@param name string
+---@param tasks? Project.Task[]
+---@param dependencies? Project.Dependency[]
+---@param commands? Project.Command[]
+---@return Project
+function Project.new(root_path, build_gradle_path, name, tasks, dependencies, commands)
+  return setmetatable({
+    id = Utils.uuid(),
+    root_path = root_path,
+    build_gradle_path = build_gradle_path,
+    name = name,
+    tasks = tasks or {},
+    dependencies = dependencies or {},
+    commands = commands or {},
+  }, Project)
+end
+
+---Set tasks
+---@param tasks Project.Task[]
+function Project:set_tasks(tasks)
+  self.tasks = tasks
+end
+
+---Set dependencies
+---@param dependencies Project.Dependency[]
+function Project:set_dependencies(dependencies)
+  self.dependencies = dependencies
+end
+
+---Set commands
+---@param commands Project.Command[]
+function Project:set_commands(commands)
+  self.commands = commands
+end
+
+---@class Project.Command
+---@field name string
+---@field description string
+---@field cmd_args string[]
+local Command = {}
+
+Command.__index = Command
+
+---@param name string
+---@param description string
+---@param cmd_args string[]
+---@return table
+function Project.Command(name, description, cmd_args)
+  local self = {}
+  setmetatable(self, Command)
+  self.name = name
+  self.description = description
+  self.cmd_args = cmd_args
+  return self
+end
+
+---@class Project.Dependency
+---@field id string
+---@field parent_id string | nil
+---@field group string
+---@field name string
+---@field version string
+---@field configuration string
+---@field is_duplicate boolean
+---@field conflict_version? string
+local Dependency = {}
+Dependency.__index = Dependency
+
+---@alias Dependency Project.Dependency
+
+---@return string
+function Dependency:get_compact_name()
+  return self.group .. ':' .. self.name .. ':' .. self.version
+end
+
+---@param id string
+---@param parent_id string | nil
+---@param group string
+---@param name string
+---@param version string
+---@param configuration string
+---@param is_duplicate? boolean
+---@param conflict_version? string
+---@return Project.Dependency
+function Project.Dependency(
+  id,
+  parent_id,
+  group,
+  name,
+  version,
+  configuration,
+  is_duplicate,
+  conflict_version
+)
+  local self = {}
+  setmetatable(self, Dependency)
+  self.id = id
+  self.parent_id = parent_id
+  self.group = group
+  self.name = name
+  self.version = version or ''
+  self.configuration = configuration
+  self.is_duplicate = is_duplicate or false
+  self.conflict_version = conflict_version
+  return self
+end
+
+---@class DependencyGroup
+---@field configuration string
+---@field dependencies Project.Dependency[]
+
+---Group dependencies by configuration name
+---@return DependencyGroup[]
+function Project:group_dependencies_by_configuration()
+  local group_by = {}
+  local last_configuration
+  for _, dependency in ipairs(self.dependencies) do
+    if dependency.configuration ~= last_configuration then
+      table.insert(
+        group_by,
+        { configuration = dependency.configuration, dependencies = { dependency } }
+      )
+      last_configuration = dependency.configuration
+    elseif last_configuration then
+      table.insert(group_by[#group_by].dependencies, dependency)
+    end
+  end
+  return group_by
+end
+
+---@class Project.Task
+---@field group string
+---@field name string
+---@field description string
+local Task = {}
+
+Task.__index = Task
+
+---@alias Task Project.Task
+
+---@param name string
+---@param description? string
+---@param group string
+---@return Project.Task
+function Project.Task(name, description, group)
+  local self = {}
+  setmetatable(self, Task)
+  self.name = name
+  self.description = description
+  self.group = group
+  return self
+end
+
+---@class DependencyGroup
+---@field group string
+---@field tasks Project.Task[]
+---
+---Group task by group name
+---@return DependencyGroup[]
+function Project:group_tasks_by_group_name()
+  local group_by = {}
+  local last_group
+  for _, task in ipairs(self.tasks) do
+    if task.group ~= last_group then
+      table.insert(group_by, { group = task.group, tasks = { task } })
+      last_group = task.group
+    elseif last_group then
+      table.insert(group_by[#group_by].tasks, task)
+    end
+  end
+  return group_by
+end
+
+return Project
