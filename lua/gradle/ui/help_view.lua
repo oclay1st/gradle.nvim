82da897601a858da7a67be475b0d09271d7b99e5
oclay <ocleidyreve@gmail.com>
2024-10-02 18:59:46 -0400

    feat: add the help view

---
+++ lua/gradle/ui/help_view.lua
@@ -0,0 +1,66 @@
+local NuiPopup = require('nui.popup')
+local NuiLine = require('nui.line')
+local event = require('nui.utils.autocmd').event
+local highlights = require('gradle.highlights')
+local GradleConfig = require('gradle.config')
+local M = {}
+
+local help_keys = {
+  { key = 'D', desc = '[Projects] analyze dependencies' },
+  { key = '/, s', desc = '[Dependencies] search' },
+  { key = '<Ctrl>s', desc = '[Dependencies] switch window' },
+  { key = '<Esc>, q', desc = 'Close' },
+}
+
+M.mount = function()
+  local popup = NuiPopup({
+    enter = true,
+    ns_id = GradleConfig.namespace,
+    relative = 'win',
+    position = '50%',
+    win_options = {
+      cursorline = false,
+      scrolloff = 2,
+      sidescrolloff = 1,
+      cursorcolumn = false,
+      colorcolumn = '',
+      spell = false,
+      list = false,
+      wrap = false,
+    },
+    border = {
+      style = 'rounded',
+      text = {
+        top = ' Gradle Help? ',
+        top_align = 'center',
+      },
+    },
+    size = {
+      width = '80%',
+      height = '20%',
+    },
+  })
+
+  popup:mount()
+
+  popup:on(event.BufLeave, function()
+    popup:unmount()
+  end)
+  popup:map('n', { '<esc>', 'q' }, function()
+    popup:unmount()
+  end)
+  local keys_header = NuiLine()
+  keys_header:append(string.format(' %14s', 'KEY(S)'), highlights.SPECIAL_TITLE)
+  keys_header:append('    ', highlights.DIM_TEXT)
+  keys_header:append('COMMAND', highlights.SPECIAL_TITLE)
+  keys_header:render(popup.bufnr, GradleConfig.namespace, 2)
+  for index, value in pairs(help_keys) do
+    local line = NuiLine()
+    line:append(string.format(' %14s', value.key), highlights.SPECIAL_TEXT)
+    line:append(' -> ', highlights.DIM_TEXT)
+    line:append(value.desc)
+    line:render(popup.bufnr, GradleConfig.namespace, index + 2)
+  end
+end
+
+return M
