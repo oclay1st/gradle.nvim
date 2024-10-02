2e9099ebbaa758a760b9bee0c7fb8f7337d0bcb6
oclay <ocleidyreve@gmail.com>
2024-10-02 18:58:19 -0400

    feat: add icons

---
+++ lua/gradle/ui/icons.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.default = {
+  package = '',
+  new_folder = '',
+  tree = '󰙅',
+  expanded = ' ',
+  collapsed = ' ',
+  gradle = '',
+  project = '',
+  tool_folder = '',
+  tool = '',
+  command = '',
+  help = '󰘥',
+  package_dependents = '',
+  package_dependencies = '',
+  warning = '',
+  entry = ' ',
+  search = '',
+}
+
+return M
