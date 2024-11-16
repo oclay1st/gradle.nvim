1f567d479229445593c9a75d9980050a9946921b
oclay <ocleidyreve@gmail.com>
2024-11-16 07:35:28 -0500

    test: load lazy path while running tests offline

--- tests/minit.lua
+++ tests/minit.lua
@@ -1,7 +1,16 @@
 #!/usr/bin/env -S nvim -l
 
 vim.env.LAZY_STDPATH = '.tests'
-load(vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'))()
+
+local ok, bootstrap = pcall(
+  vim.fn.system,
+  'curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'
+)
+if ok then
+  load(bootstrap)()
+else
+  vim.opt.rtp:prepend(vim.env.LAZY_STDPATH)
+end
 
 -- Setup lazy.nvim
 require('lazy.minit').busted({
